f8f0ad9a1760afc3ec1fb9412f9c66ae1a89aae5
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-04-22 14:34:09 -0300

    oooo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -38,7 +38,7 @@ app.post("/register", async (req, res) => {
   try {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password,
+      password: md5(req.body.password),
     });
 
     await newUser.save();
